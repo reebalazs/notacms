fe22590e9993d905b42727b61d516ba859a5dc7a
Balazs Ree <ree@greenfinity.hu>
2015-02-16 17:13:29 +0100

    Fix sticky footer directive

--- src/gf-footer-directive/gf-footer-directive.js
+++ src/gf-footer-directive/gf-footer-directive.js
@@ -8,41 +8,40 @@ var $ = require('jquery');
 module.exports = ['$window', '$timeout',
 function($window, $timeout) {
 
-  var elStatic, timer;
+  var elGhost, timer;
 
   function adjustStickyPosition(scope, el, attr) {
     // measure positions
-    el = $(el);
-    var needsToStick = el.offset().top < elStatic.offset().top;
-    if (needsToStick) {
-      el.css('opacity', 0);
-      elStatic.css('opacity', 1);
-    } else {
-      el.css('opacity', 1);
-      elStatic.css('opacity', 0);
-    }
+    var $w = $($window);
+    var viewPortBottom = $w.scrollTop() + $w.height();
+    var ghostBottom = elGhost.offset().top + elGhost.outerHeight();
+    var needsToStick = ghostBottom < viewPortBottom;
+    $(el).css('position', needsToStick ? 'fixed' : 'static');
   }
 
   function adjustStickyDelayed(scope, el, attr) {
-    if (timer) {
-      $timeout.cancel(timer);
-    }
-    timer = $timeout(function() {
+    if (!timer) {
       adjustStickyPosition(scope, el, attr);
-    }, 200);
+      // Hold off next adjustments until a delay,
+      // to avoid congesting up resize.
+      timer = $timeout(function() {
+        // Retry after the delay.
+        adjustStickyPosition(scope, el, attr);
+        timer = null;
+      }, 100);
+    }
   }
 
   function link(scope, el, attr) {
-    elStatic = $(el.clone(true))
+    elGhost = $(el.clone(true))
       .css({
-        position: 'fixed',
-        bottom: 0,
-        width: '100%'
-      }).removeClass('row')
+        opacity: 0
+      })
       .insertAfter(el);
+    el.css('bottom', 0);
     var f = $.proxy(adjustStickyDelayed, null, scope, el, attr);
-    $($window).bind('scroll resize', f);
-    scope.$on('scroll.manually', f);
+    $($window).bind('resize', f);
+    scope.$on('resize.manually', f);
     f();
   }
 
